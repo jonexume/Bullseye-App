5608cd84fb0a7f653bed0bb476965458ebad88a3
Jon Exume <jonexume@me.com>
2020-07-04 18:55:25 -0400

    Added all the elements to the app.  Will push a version up to GitHub.

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var currentValue: Int = 50
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,13 +18,20 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello World", message: "This is my first app I made", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        let message = "The value of the slider is: \(currentValue)"
+        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK Dokey", style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
     }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+
 
 }
 
